851d36752ac630dcd8e363d8f72b09f762913b63
Zzzjm1919 <zjm984106224@outlook.com>
2025-06-24 18:44:56 +0800

    refactor(storage-memory): update directory creation logic and remove unused functions
    - Update MkdirAll calls to use os.ModeDir instead of hardcoded permissions
    - Remove unused functions: CreateDir, DeleteDir, CreateFile, DeleteFile

--- internal/remote/storage/storage_memory/client.go
+++ internal/remote/storage/storage_memory/client.go
@@ -41,59 +41,31 @@ func Init() {
 // EnsureDir Ensure that the directory exists (create if it does not exist)
 func (fc *FileClient) EnsureDir() error {
 	absPath := filepath.Join(fc.dir)
-	err := os.MkdirAll(absPath, 0755)
+	err := os.MkdirAll(absPath, os.ModeDir)
 	if err != nil {
 		log.Warnf("warn create storage dir err: %v", err)
 	}
 	kvPath := filepath.Join(absPath, keyValueDir)
-	err = os.MkdirAll(kvPath, 0755)
+	err = os.MkdirAll(kvPath, os.ModeDir)
 	if err != nil {
 		log.Warnf("warn create storage dir err: %v", err)
 	}
 	queuePath := filepath.Join(absPath, queueDir)
-	err = os.MkdirAll(queuePath, 0755)
+	err = os.MkdirAll(queuePath, os.ModeDir)
 	if err != nil {
 		log.Warnf("warn create storage dir err: %v", err)
 	}
 	datasetPath := filepath.Join(absPath, datasetDir)
-	err = os.MkdirAll(datasetPath, 0755)
+	err = os.MkdirAll(datasetPath, os.ModeDir)
 	if err != nil {
 		log.Warnf("warn create storage dir err: %v", err)
 	}
 	objectPath := filepath.Join(absPath, objectDir)
-	err = os.MkdirAll(objectPath, 0755)
+	err = os.MkdirAll(objectPath, os.ModeDir)
 	if err != nil {
 		log.Warnf("warn create storage dir err: %v", err)
 	}
 	return err
-}
-
-// CreateDir Create folder (recursive creation)
-func (fc *FileClient) CreateDir(relPath string) error {
-	absPath := filepath.Join(fc.dir, relPath)
-	return os.MkdirAll(absPath, 0755)
-}
-
-// DeleteDir Delete the entire folder and its contents
-func (fc *FileClient) DeleteDir(relPath string) error {
-	absPath := filepath.Join(fc.dir, relPath)
-	return os.RemoveAll(absPath)
-}
-
-// CreateFile Create file and write content (automatically create directory)
-func (fc *FileClient) CreateFile(relPath string, content []byte) error {
-	absPath := filepath.Join(fc.dir, relPath)
-	dir := filepath.Dir(absPath)
-	if err := os.MkdirAll(dir, 0755); err != nil {
-		return err
-	}
-	return os.WriteFile(absPath, content, 0644)
-}
-
-// DeleteFile Delete the specified file
-func (fc *FileClient) DeleteFile(relPath string) error {
-	absPath := filepath.Join(fc.dir, relPath)
-	return os.Remove(absPath)
 }
 
 type metadata struct {
