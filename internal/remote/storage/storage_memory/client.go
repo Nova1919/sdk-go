7c440b21524a94abe17d1d1dde8b751863d292c1
Epiphany <smash.qi.huang@gmail.com>
2025-06-25 19:06:52 +0800

    feat(storage_memory): implement metadata and directory creation for local storage

--- internal/remote/storage/storage_memory/client.go
+++ internal/remote/storage/storage_memory/client.go
@@ -1,9 +1,12 @@
 package storage_memory
 
 import (
+	"encoding/json"
+	"github.com/smash-hq/sdk-go/internal/remote/storage/models"
 	"github.com/smash-hq/sdk-go/scrapeless/log"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 var storageDir string
@@ -43,13 +46,81 @@ func Default() *LocalClient {
 // EnsureDir Ensure that the directory exists (create if it does not exist)
 func EnsureDir(storageDir string) error {
 	absPath := filepath.Join(storageDir)
-	err := os.MkdirAll(absPath, os.ModeDir)
-	if err != nil {
-		log.Warnf("warn create storage dir err: %v", err)
+	err := createRoot(absPath)
+	path, err := createDir(absPath, keyValueDir)
+	createMetadata(path, keyValueDir)
+	path, err = createDir(absPath, queueDir)
+	createMetadata(path, queueDir)
+	path, err = createDir(absPath, datasetDir)
+	createMetadata(path, datasetDir)
+	path, err = createDir(absPath, objectDir)
+	createMetadata(path, objectDir)
+	createInput(absPath)
+	return err
+}
+
+func createMetadata(path, category string) {
+	metaPath := filepath.Join(path, metadataFile)
+	var meta []byte
+	var def = "default"
+	var now = time.Now().Format(time.RFC3339)
+	switch category {
+	case datasetDir:
+		datasetData := models.Dataset{
+			Id:         def,
+			Name:       def,
+			ActorId:    def,
+			RunId:      def,
+			CreatedAt:  def,
+			UpdatedAt:  now,
+			AccessedAt: now,
+			Stats:      models.DatasetStats{},
+		}
+		meta, _ = json.MarshalIndent(datasetData, "", "  ")
+	case keyValueDir:
+		kvData := models.KvNamespaceItem{
+			Id:        def,
+			Name:      def,
+			RunId:     def,
+			ActorId:   def,
+			CreatedAt: now,
+			UpdatedAt: now,
+		}
+		meta, _ = json.MarshalIndent(kvData, "", "  ")
+	case queueDir:
+		queue := models.Queue{
+			Id:          def,
+			Name:        def,
+			TeamId:      def,
+			ActorId:     def,
+			RunId:       def,
+			Description: def,
+			CreatedAt:   now,
+			UpdatedAt:   now,
+			Stats:       models.QueueStats{},
+		}
+		meta, _ = json.MarshalIndent(queue, "", "  ")
+	case objectDir:
+		bucket := models.Bucket{
+			Id:          def,
+			Name:        def,
+			Description: def,
+			CreatedAt:   now,
+			UpdatedAt:   now,
+			ActorId:     def,
+			RunId:       def,
+			Size:        0,
+		}
+		meta, _ = json.MarshalIndent(bucket, "", "  ")
 	}
-	kvPath := filepath.Join(absPath, keyValueDir, defaultDir)
-	err = os.MkdirAll(kvPath, os.ModeDir)
-	inputPath := filepath.Join(kvPath, inputJson)
+	err := os.WriteFile(metaPath, meta, os.ModePerm)
+	if err != nil {
+		log.Warnf("warn create metadata.json failed: %v", err)
+	}
+}
+
+func createInput(absPath string) {
+	inputPath := filepath.Join(absPath, keyValueDir, defaultDir, inputJson)
 	if !isFileExists(inputPath) {
 		create, err := os.Create(inputPath)
 		defer create.Close()
@@ -57,25 +128,23 @@ func EnsureDir(storageDir string) error {
 			log.Warnf("create INPUT.json failed: %v", err)
 		}
 	}
-	if err != nil {
-		log.Warnf("warn create storage dir err: %v", err)
-	}
-	queuePath := filepath.Join(absPath, queueDir, defaultDir)
-	err = os.MkdirAll(queuePath, os.ModeDir)
-	if err != nil {
-		log.Warnf("warn create storage dir err: %v", err)
-	}
-	datasetPath := filepath.Join(absPath, datasetDir, defaultDir)
-	err = os.MkdirAll(datasetPath, os.ModeDir)
-	if err != nil {
-		log.Warnf("warn create storage dir err: %v", err)
-	}
-	objectPath := filepath.Join(absPath, objectDir, defaultDir)
-	err = os.MkdirAll(objectPath, os.ModeDir)
+}
+
+func createRoot(absPath string) error {
+	err := os.MkdirAll(absPath, os.ModeDir)
 	if err != nil {
 		log.Warnf("warn create storage dir err: %v", err)
 	}
 	return err
+}
+
+func createDir(absPath string, dirName string) (string, error) {
+	path := filepath.Join(absPath, dirName, defaultDir)
+	err := os.MkdirAll(path, os.ModeDir)
+	if err != nil {
+		log.Warnf("warn create dir err: %v", err)
+	}
+	return path, err
 }
 
 func (c *LocalClient) Close() error {
