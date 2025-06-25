dd4b8fa38a74ececf4d6ca7f3cc814abf1b7b379
Epiphany <smash.qi.huang@gmail.com>
2025-06-25 15:36:53 +0800

    feat: implement object storage operations

--- internal/remote/storage/storage_memory/object.go
+++ internal/remote/storage/storage_memory/object.go
@@ -1,3 +1,38 @@
 package storage_memory
 
-// todo Object File Operation
+import (
+	"context"
+	"github.com/scrapeless-ai/sdk-go/internal/remote/storage/models"
+)
+
+func (d *LocalClient) ListBuckets(ctx context.Context, page, size int) (*models.Object, error) {
+	return nil, nil
+}
+
+func (d *LocalClient) CreateBucket(ctx context.Context, req *models.CreateBucketRequest) (string, error) {
+	return "", nil
+}
+
+func (d *LocalClient) DeleteBucket(ctx context.Context, bucketId string) (bool, error) {
+	return false, nil
+}
+
+func (d *LocalClient) GetBucket(ctx context.Context, bucketId string) (*models.Bucket, error) {
+	return nil, nil
+}
+
+func (d *LocalClient) ListObjects(ctx context.Context, req *models.ListObjectsRequest) (*models.ObjectList, error) {
+	return nil, nil
+}
+
+func (d *LocalClient) GetObject(ctx context.Context, req *models.ObjectRequest) ([]byte, error) {
+	return nil, nil
+}
+
+func (d *LocalClient) DeleteObject(ctx context.Context, req *models.ObjectRequest) (bool, error) {
+	return false, nil
+}
+
+func (d *LocalClient) PutObject(ctx context.Context, req *models.PutObjectRequest) (string, error) {
+	return "", nil
+}
